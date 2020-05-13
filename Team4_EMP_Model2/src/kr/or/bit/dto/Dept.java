5a5ecefb4749da3f8d139bf9b08fb1daeed72c0f
test_git <geranium236@gmail.com>
2020-05-13 18:49:51 +0900

    dept 빠져있어서 추가

--- Team4_EMP_Model2/src/kr/or/bit/dto/Dept.java
+++ Team4_EMP_Model2/src/kr/or/bit/dto/Dept.java
@@ -1,5 +1,28 @@
 package kr.or.bit.dto;
 
 public class Dept {
-
+	
+	private int deptno;
+	private String dname,loc;
+	
+	
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	
 }
