a6bbaa44efc2032408a3d10c46fb8bf505188add
bocastle <bocastle1213@gmail.com>
2020-05-25 14:15:50 +0900

    no message

--- Book4zo/src/kr/or/book4zo/action/Action.java
+++ Book4zo/src/kr/or/book4zo/action/Action.java
@@ -1,8 +1,14 @@
 package kr.or.book4zo.action;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 public interface Action {
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response); 
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception; //서블릿이 가진 함수의 파라미터와 똑같다. 
+	
 }
